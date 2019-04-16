e6ddcfa1ab59d2602c946bd6bacaac5d7f46c129
Siarhei Babich <Siarhei.Babich@gmail.com>
2019-04-16 20:59:40 +0300

    Two Sum solution

---
+++ src/main/java/codewars/java/Solution.java
@@ -0,0 +1,16 @@
+package codewars.java;
+
+public class Solution {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length && j != i; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+}
